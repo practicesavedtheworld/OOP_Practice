e57bd2b5b5f7eaa47e9e8e33aa95840e2e151122
practicesavedtheworld <richdaysiscoming@gmail.com>
2022-06-07 17:44:51 +0300

    final version without tests

--- problem1.py
+++ problem1.py
@@ -1,3 +1,15 @@
+"""
+Объявите класс с именем Notes и определите в нем, следующие атрибуты, без использования конструктора:
+
+uid: 1005435
+title: "Шутка"
+author: "И.С. Бах"
+pages: 2
+
+Затем, с помощью функции getattr() прочитайте и выведите на экран значение атрибута author.
+"""
+
+
 class Notes:
     pass
 
@@ -8,5 +20,7 @@ d = {
     'author': "И.С. Бах",
     'pages': 2
 }
-[setattr(Notes, k, v) for k, v in d.items()]
-print(getattr(Notes, 'author'))
+[setattr(Notes, key, value) for key, value in d.items()]
+
+if __name__ == '__main__':
+    print(getattr(Notes, 'author'))
