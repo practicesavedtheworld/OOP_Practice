13dc148545a88b5b666bd959727c15f60676742a
practicesavedtheworld <richdaysiscoming@gmail.com>
2022-06-09 20:07:56 +0300

    info

--- LowLevelTasks/problem3.py
+++ LowLevelTasks/problem3.py
@@ -1,46 +1,37 @@
-"""
-Объявите три класса геометрических фигур: Line, Rect, Ellipse. Должна быть возможность создавать объекты
-каждого класса, следующими командами:
-
-g1 = Line(a, b, c, d)
-g2 = Rect(a, b, c, d)
-g3 = Ellipse(a, b, c, d)
-
-Здесь в качестве аргументов a, b, c, d передаются координаты верхнего правого и нижнего левого
-углов (произвольные числа). В каждом объекте координаты должны сохраняться в локальных
-свойствах sp (верхний правый угол) и ep (нижний левый) в виде кортежей (a, b) и (c, d) соответственно.
-
-Сформируйте 217 объектов этих классов: для каждого текущего объекта класс
-выбирается случайно (или Line, или Rect, или Ellipse). Координаты также генерируются
-случайным образом (числовые значения). Все объекты сохраните в списке elements.
-
-В списке elements обнулите координаты объектов только для класса Line.
-"""
-from random import sample, choice
+from random import choice
 
 
-class Line:
-
-    def __init__(self, a, b, c, d):
-        self.sp = (a, b)
-        self.ep = (c, d)
-
-
-class Rect:
-
-    def __init__(self, a, b, c, d):
-        self.sp = (a, b)
-        self.ep = (c, d)
+class Cell:
+    def __init__(self, mine: bool, around_mines=0, fl_open=False):
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open = fl_open
 
 
-class Ellipse:
+class GamePole:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        self.pole = [[Cell(choice([*range(1, 6)]), False).around_mines, Cell(choice([*range(1, 6)]), False).mine]
+                for _ in range(self.n) for _ in range(self.n)]
+        self.init()
 
-    def __init__(self, a, b, c, d):
-        self.sp = (a, b)
-        self.ep = (c, d)
+    def init(self):
 
 
-if __name__ == '__main__':
-    elements = [choice([Line(0, 0, 0, 0),
-                        Rect(*sample(range(22), 4)),
-                        Ellipse(*sample(range(6), 4))]) for _ in range(217)]
+        for i, el in enumerate(self.pole):
+            if i == self.m:
+                break
+            self.pole[choice([*range(1, len(self.pole))])] = [Cell(choice([*range(1, 6)]), True).around_mines,
+                                                    Cell(choice([*range(1, 6)]), True).mine]
+        return self.pole
+
+    def show(self):
+        for i in self.init():
+            print(i)
+
+
+# c1 = Cell(choice([*range(1, 6)]), choice([False, True]))
+
+pole_game = GamePole(10, 12)
+pole_game.show()
