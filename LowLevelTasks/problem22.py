93ced3d276e7ad396abf56ce62b9e31799c53e32
practicesavedtheworld <richdaysiscoming@gmail.com>
2022-07-14 11:32:59 +0300

    problem22

--- LowLevelTasks/problem22.py
+++ LowLevelTasks/problem22.py
@@ -53,3 +53,62 @@ modules - список модулей в курсе (изначально спи
 add_module(self, module) - добавление нового модуля в конце списка modules;
 remove_module(self, indx) - удаление модуля из списка modules по индексу в этом списке.
 """
+
+
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.modules = []
+
+    def add_module(self, module):
+        self.modules.append(module)
+
+    def remove_module(self, indx):
+        del self.modules[indx - 1]
+
+
+class Module:
+    def __init__(self, name):
+        self.name = name
+        self.lessons = []
+
+    def add_lesson(self, lesson):
+        self.lessons.append(lesson)
+
+    def remove_lesson(self, indx):
+        del self.lessons[indx - 1]
+
+
+class LessonItem:
+    def __init__(self, title: str, practices: int, duration: int):
+        self.title = title
+        self.practices = practices
+        self.duration = duration
+
+    def __setattr__(self, key, value):
+        d = {
+            'title': str, 'practices': int, 'duration': int
+        }
+        if not isinstance(value, d.get(key)):
+            raise TypeError("Неверный тип присваиваемых данных.")
+        super().__setattr__(key, value)
+
+    def __getattr__(self, name):
+        return False
+
+    def __delattr__(self, name):
+        if name in self.__dict__:
+            raise AttributeError("Атрибут удалять запрещено.")
+
+
+if __name__ == '__main__':
+    course = Course("Python ООП")
+    module_1 = Module("Часть первая")
+    module_1.add_lesson(LessonItem("Урок 1", 7, 1000))
+    module_1.add_lesson(LessonItem("Урок 2", 10, 1200))
+    module_1.add_lesson(LessonItem("Урок 3", 5, 800))
+    course.add_module(module_1)
+    module_2 = Module("Часть вторая")
+    module_2.add_lesson(LessonItem("Урок 1", 7, 1000))
+    module_2.add_lesson(LessonItem("Урок 2", 10, 1200))
+    course.add_module(module_2)
