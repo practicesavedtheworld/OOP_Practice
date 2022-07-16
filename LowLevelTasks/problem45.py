c0398a2592198524afaa2ab19a5fcad84f61ded3
practicesavedtheworld <richdaysiscoming@gmail.com>
2022-07-16 17:04:00 +0300

    problem45

---
+++ LowLevelTasks/problem45.py
@@ -0,0 +1,34 @@
+"""
+Объявите класс PolyLine (полилиния) для представления линии из последовательности прямолинейных сегментов.
+Объекты этого класса должны создаваться командой:
+
+poly = PolyLine(start_coord, coord_2, coord_3, ..., coord_N)
+
+Здесь start_coord - координата начала полилинии (кортеж из двух чисел x, y);
+coord_2, coord_3, ... - последующие координаты точек на плоскости (представленные кортежами), соединенных прямыми линиями.
+
+Например:
+
+poly = PolyLine((1, 2), (3, 5), (0, 10), (-1, 8))
+
+В классе PolyLine должны быть объявлены следующие методы:
+
+add_coord(x, y) - добавление новой координаты (в конец);
+remove_coord(indx) - удаление координаты по индексу (порядковому номеру, начинается с нуля);
+get_coords() - получение списка координат (в виде списка из кортежей).
+"""
+
+
+class PolyLine:
+    def __init__(self, start_coord, *args):
+        self.start_coord = start_coord
+        self.coords = [start_coord] + [el for el in args]
+
+    def add_coord(self, x, y):
+        self.coords.append((x, y))
+
+    def remove_coord(self, indx):
+        del self.coords[indx]
+
+    def get_coords(self):
+        return self.coords
